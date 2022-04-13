e160ea8264e1d6bfc74a32670f39afdb74ee5d4f
siraom15 <siriwatjaiyungyuen@gmail.com>
2022-04-13 19:41:43 +0700

    [Bew] Add Generate Color

--- src/components/GenColorList.vue
+++ src/components/GenColorList.vue
@@ -1,22 +1,42 @@
 <script setup>
-defineEmits(['deleteColor', 'editColor'])
-defineProps({
-    colorList: {
-        type: Array,
-        require: true
-    }
-})
+import { onBeforeMount, ref } from "@vue/runtime-core";
+import { useRoute } from "vue-router";
+const route = useRoute();
+defineEmits(["deleteColor", "editColor"]);
+const props = defineProps({
+  colorList: {
+    type: Array,
+    require: true,
+  },
+});
+const colorList = ref({});
+
+const getColorFromDb = async (colorId) => {
+  const res = await fetch(`http://localhost:5000/colors/${colorId}`);
+  if (res.status === 200) {
+    let data = await res.json();
+    console.log(data);
+    colorList.value = data.colorPalettes;
+    console.log(colorList.value);
+  } else {
+    console.log("not get colorList");
+  }
+};
+onBeforeMount(async () => {
+  if (route.query.colorExploreId) {
+    await getColorFromDb(route.query.colorExploreId);
+  }
+});
 </script>
- 
+
 <template>
-    <div v-for="(color, index) in colorList" :key="index">
-        <div v-bind:style="{ 'background-color': color }">&nbsp;</div>
-        <button @click="$emit('editColor', color)">Edit</button>
-        <button @click="$emit('deleteColor', color.id)">Delete</button>
-        <!-- <button @click="addColorToList(index)">edit</button> &nbsp;
+  <div v-for="(color, index) in colorList" :key="index">
+    <div v-bind:style="{ 'background-color': color }">&nbsp;</div>
+    <button @click="$emit('editColor', color)">Edit</button>
+    <button @click="$emit('deleteColor', color.id)">Delete</button>
+    <!-- <button @click="addColorToList(index)">edit</button> &nbsp;
         <button @click="deleteColorList(index)">delete</button>-->
-    </div>
+  </div>
 </template>
- 
-<style>
-</style>
+
+<style></style>
--- src/views/ExploreColor.vue
+++ src/views/ExploreColor.vue
@@ -1,39 +1,33 @@
 <script setup>
-import { onBeforeMount,ref } from 'vue';
-import ExploreList from '../components/ExploreList.vue';
+import { onBeforeMount, ref } from "vue";
 
+import ExploreList from "../components/ExploreList.vue";
 const colors = ref([]);
-const getColors= async () => {
-  const res = await fetch('http://localhost:5000/colors') //มี await ต้องเปลี่ยนฟังก์ชั่นให้เป็น async
+const getColors = async () => {
+  const res = await fetch("http://localhost:5000/colors"); //มี await ต้องเปลี่ยนฟังก์ชั่นให้เป็น async
   if (res.status === 200) {
-    colors.value = await res.json() //json() getdata + covert to Js Obj.
+    colors.value = await res.json(); //json() getdata + covert to Js Obj.
     console.log(colors.value);
-  }
-  else
-    console.log('not get colors');
-}
+  } else console.log("not get colors");
+};
 onBeforeMount(async () => {
-  await getColors()
-})
-// const genColor = ref({})
-// const togenerateMode = (toGenColor) => {
-//     genColor.value = toGenColor
-//     console.log(genColor.value);
-// }
+  await getColors();
+});
 
 </script>
 
 <template>
-    <div class="text-center">
-        <p class="text-2xl">Trending Color Palettes</p>
-        <p>Get inspired by thousands of beatiful color schemes and make something cool!</p>
-        <br />
-        <div>
-            <ExploreList :exploreList="colors" />
-            <!-- <ExploreList :exploreList="colors" @addGenColor="togenerateMode" /> -->
-        </div>
+  <div class="text-center">
+    <p class="text-2xl">Trending Color Palettes</p>
+    <p>
+      Get inspired by thousands of beatiful color schemes and make something
+      cool!
+    </p>
+    <br />
+    <div>
+      <ExploreList :exploreList="colors" />
     </div>
+  </div>
 </template>
- 
-<style>
-</style>
+
+<style></style>
