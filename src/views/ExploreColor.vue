d8fa12d0f73f0334763ddbf8f9b5730f63e2355c
bewburats <bew25442@gmail.com>
2022-04-13 16:00:39 +0700

    [B]พังแล้ว บาย

--- src/views/ExploreColor.vue
+++ src/views/ExploreColor.vue
@@ -1,16 +1,8 @@
 <script setup>
 import { onBeforeMount,ref } from 'vue';
 import ExploreList from '../components/ExploreList.vue';
-// const colorExplores = 
-// [
-//     { id: 1, colorPalettes: ['#d8e2dc', '#ffe5d9', '#ffcad4', '#f4acb7', '#9d8189'] },
-//     { id: 2, colorPalettes: ['#f6bd60', '#f7ede2', '#f5cac3', '#84a59d', '#f28482'] },
-//     { id: 3, colorPalettes: ['#0fa3b1', '#d9e5d6', '#eddea4', '#f7a072', '#ff9b42'] },
-//     { id: 4, colorPalettes: ['#1f2421', '#216869', '#49a078', '#9cc5a1', '#dce1de'] },
-//     { id: 5, colorPalettes: ['#dcdcdd', '#c5c3c6', '#46494c', '#4c5c68', '#1985a1'] },
-//     { id: 6, colorPalettes: ['#1a1423', '#372549', '#774c60', '#b75d69', '#eacdc2'] }
-// ]
 
+const colors = ref([]);
 const getColors= async () => {
   const res = await fetch('http://localhost:5000/colors') //มี await ต้องเปลี่ยนฟังก์ชั่นให้เป็น async
   if (res.status === 200) {
@@ -23,11 +15,11 @@ const getColors= async () => {
 onBeforeMount(async () => {
   await getColors()
 })
-const genColor = ref({})
-const togenerateMode = (toGenColor) => {
-    genColor.value = toGenColor
-    console.log(genColor.value);
-}
+// const genColor = ref({})
+// const togenerateMode = (toGenColor) => {
+//     genColor.value = toGenColor
+//     console.log(genColor.value);
+// }
 
 </script>
 
@@ -37,7 +29,8 @@ const togenerateMode = (toGenColor) => {
         <p>Get inspired by thousands of beatiful color schemes and make something cool!</p>
         <br />
         <div>
-            <ExploreList :exploreList="colors" @addGenColor="togenerateMode" />
+            <ExploreList :exploreList="colors" />
+            <!-- <ExploreList :exploreList="colors" @addGenColor="togenerateMode" /> -->
         </div>
     </div>
 </template>
